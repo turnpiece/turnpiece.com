96a1270e7fdc4a1fc265ee1e00ded76a5dc34af2
Paul Jenkins <paul@turnpiece.com>
2025-06-30 15:57:33 +0100

    adding whitenoise and removing redundant /assets folder

--- turnpiece/settings.py
+++ turnpiece/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +138,7 @@ STATICFILES_DIRS = [
 
 # Static files for production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
