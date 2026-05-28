4a552bbf377d47e6ad0ec0ceecbe69022b4a2796
Paul Jenkins <paul@turnpiece.com>
2026-05-28 16:01:49 +0100

    DEBUG defaults to False

--- turnpiece/settings.py
+++ turnpiece/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
